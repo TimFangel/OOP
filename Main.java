5cdaf1af68a0b8e65bdc73333182f718ad523793
TimFangel <tfni24@student.aau.dk>
2025-10-09 11:51:38 +0200

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,28 +1,15 @@
-public class main {
-    	public static void main(String[] args) { 
-		A a = new B(); 
-		a.m1(); 
-		a.m2(); 
-		((B)a).m2(); 
-		A.m2(); 
-		B.m2(); 
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) { 
+	
+		Day[] days = {Day.FRIDAY, Day.MONDAY, Day.WEDNESDAY};
+		System.out.println(Arrays.toString(days)); 
+		Arrays.sort(days); 
+		System.out.println(Arrays.toString(days));
 	} 
 }
 
-class A { 
-	public void m1() { 
-		System.out.println("A.m1()"); 
-	} 
-	public static void m2() { 
-		System.out.println("A.m2()"); 
-	} 
+enum Day { 
+	MONDAY, TUESDAY, WEDNESDAY, THURSDAY, FRIDAY, SATURDAY, SUNDAY; 
 } 
-class B extends A { 
-	@Override 
-	public void m1() { 
-		System.out.println("B.m1()"); 
-	} 
-	public static void m2() { 
-		System.out.println("B.m2()"); 
-	} 
-}
